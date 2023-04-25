edbbad9a1a80a4c9546f957afc5bc0b63e3d8432
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2023-04-25 07:49:46 +0000

    Created a test file for the rotating stage fsm

---
+++ copy.py
@@ -0,0 +1,67 @@
+from collections import OrderedDict
+
+SCENARIOS = OrderedDict([
+            (
+                1,
+                    [
+                        {
+                            'status': 'On' #this can be derived from the dividers
+                            'transition_time':10,
+                            'effects_on_enter':['light_fade_in', 
+                                       'light_fade_out',
+                                       'animal_sound'
+                                      ],
+                            'effects_on_exit':['draw_curtain'],
+                            'effects_after': {'10':'flicker_light'},
+                            'transition_event': 'listen_for_cues',
+                            'priority': ['transition_event', 'transition_time'],
+                            'response_speed': 'standard'
+                            'record': ['video', 'audio']
+                        
+                        }
+                    ]
+            ),
+
+            (
+                2,
+
+                    {
+                        'status': 'Off'
+                    }
+
+
+            ),
+
+            (
+                3,
+
+                    {
+                        'status': 'Off'
+                    }
+
+
+            ),
+
+            (
+                4,
+
+                    {
+                        'status': 'Off'
+                    }
+
+
+            )
+
+])
+
+PINS = OrderedDict([
+    (
+        #"GPIO_POOL",
+        #[0,2,3,4,5,12,13,14,15,18,19,21,22,23,25,26,27,32,33]# for esp32 38 pin config
+        "GPIO_POOL",
+        [13,14,12,27,33,26,15,4,18,23,22,25] #for esp32 30 pin config
+
+    )
+
+
+])
---
+++ test_fsm_operation.py
@@ -0,0 +1,11 @@
+import unittest
+from rotate_stage_fsm import Transition, State, StateMachine  
+
+class TestFsm(unittest.TestCase):
+  """
+  Test the operations of the fsm - The number of states
+                                 - The Transition
+                                 - The StateMachine
+                                 - The State
+  """
+  pass
