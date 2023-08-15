0a46988204d2fdfd12d44571433a30b8608ae370
Ahmad Usman <ahmadujuwon@gmail.com>
2023-08-15 13:34:19 +0100

    fix: curtain code

--- curtain.py
+++ curtain.py
@@ -1,19 +1,20 @@
 class Curtain():
     def __init__():
         pass
-    
+
     def close():
         print('curtain closed...')
 
     def draw():
         print('curtain fully opened')
-    
+
     def open_to(width):
         pass
-    
+
 
 from math import sin, pi
 from motor import Motor
+
 class CurtainNew():
     #  always note the units you're working with, rad, degree, m, cm
     def __init__(self, motor_pins, divisions, stage_radius, motor_radius):
@@ -21,23 +22,37 @@ class CurtainNew():
         angle_per_div = (2 * pi) / divisions
         self.secant_length = (2 * stage_radius) * sin(angle_per_div / 2)
         self.motor_radius = motor_radius
+        # self.angle_per_cm = 90.56604
+        self.angle_per_cm = 90
+
 
         #test to see if this works
-        # To get the angle the motor needs to move to move the curtain along teh secant length
+        # To get the angle the motor needs to move to move the curtain along the secant length
         # each curtain needs to move an angle of angle_per_secant / 2
+
+        # I think this would work better when we know the motor radius
         self.angle_per_secant = (self.secant_length / motor_radius) * (180 / pi)
 
 
     def close(self):
+        # self.motor.rotate_by(self.angle_per_secant / 2, reverse=True)
+        self.open_to(self.secant_length, reverse=True)
         print('curtain closed...')
-        self.motor.rotate_by(self.angle_per_secant / 2, reverse=True)
 
     def draw(self):
+        # self.motor.rotate_by(self.angle_per_secant / 2)
+        self.open_to(self.secant_length)
         print('curtain fully opened')
-        self.motor.rotate_by(self.angle_per_secant / 2)
 
     def open_to(self, width, reverse=False):
-        print("Moving by" + str(width))
-        angle_to_move = (width / self.motor_radius) * (180 / pi)
+        print(f"{'Closing' if reverse else 'Opening'} by" + str(width))
+        angle_to_move = width * self.angle_per_cm
+        print("Opening by angle", angle_to_move)
         self.motor.rotate_by(angle_to_move, reverse)
 
+# Suggestions by Mr Eniola
+# measure the circumference of the gear
+# relate circumference to the angle of rotation
+
+# you can rotate the gear maybe 4 times and measure the distance the belt travelled. Then use that estimate
+# the distance covered by one rotation(i.e 360 deg)
--- curtain_test.py
+++ curtain_test.py
@@ -5,4 +5,4 @@ from config import PINS
 if __name__ == "__main__":
     print("HEYYY")
     stage_curtain = CurtainNew(PINS, 4, 15, 0.7)
-    stage_curtain.open_to(100)
+    stage_curtain.open_to(10, True)
